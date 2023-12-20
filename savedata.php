e48678dc5fce920dd37768a1120fefd8e7c948f2
EllenaAmanda <ellenaamanda846@gmail.com>
2023-12-20 11:04:11 +0700

    commit change

--- savedata.php
+++ savedata.php
@@ -6,11 +6,11 @@ $age=$_POST['age'];
 $gender=$_POST['gender'];
 $blood_group=$_POST['blood'];
 $address=$_POST['address'];
-$conn=mysqli_connect("localhost","id21483901_projekcc","testingCC123*","id21483901_bloodbankdb") or die("Connection error");
+$conn=mysqli_connect("sql12.freesqldatabase.com","sql12671714","PkgXzm3Scs","sql12671714") or die("Connection error");
 $sql= "INSERT INTO donor_details(donor_name,donor_number,donor_mail,donor_age,donor_gender,donor_blood,donor_address) values('{$name}','{$number}','{$email}','{$age}','{$gender}','{$blood_group}','{$address}')";
 $result=mysqli_query($conn,$sql) or die("query unsuccessful.");
 
-header("Location: https://donorprojekcc.000webhostapp.com/BDMS/home.php");
+header("Location: https://bloodbankusu.azurewebsites.net/index.php");
 
 mysqli_close($conn);
  ?>
